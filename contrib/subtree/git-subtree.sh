e0f656bee8dda69b1690d8b8ab318dabf033b024
Tom Clarkson <tom@tqclarkson.com>
2019-12-19 15:45:34 +1100

    subtree: add git subtree use and ignore commands
    Tell split to use or ignore larger sections of the history. In most cases
    split does this automatically based on metadata from subtree add.
    
    Signed-off-by: Tom Clarkson <tom@tqclarkson.com>
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- contrib/subtree/git-subtree.sh
+++ contrib/subtree/git-subtree.sh
@@ -33,6 +33,8 @@ git subtree merge --prefix=<prefix> <commit>
 git subtree split --prefix=<prefix> [<commit>]
 git subtree pull  --prefix=<prefix> <repository> <ref>
 git subtree push  --prefix=<prefix> <repository> <refspec>
+git subtree ignore --prefix=<prefix> <commit>
+git subtree use    --prefix=<prefix> <commit>
 --
 h,help        show the help
 q             quiet
@@ -140,7 +142,7 @@ main () {
 		allow_split=1
 		allow_addmerge=$arg_split_rejoin
 		;;
-	map)
+	map|ignore|use)
 		;;
 	*)
 		die "Unknown command '$arg_command'"
@@ -495,6 +497,18 @@ find_mainline_ref () {
 			;;
 		esac
 	done
+}
+
+exclude_processed_refs () {
+		if test -r "$cachedir/processed"
+		then
+			cat "$cachedir/processed" |
+			while read rev
+			do
+				debug "read $rev"
+				echo "^$rev"
+			done
+		fi
 }
 
 # Usage: copy_commit REV TREE FLAGS_STR
@@ -901,18 +915,60 @@ cmd_add_commit () {
 
 # Usage: cmd_map OLDREV NEWREV
 cmd_map () {
-	oldrev="$1"
-	newrev="$2"
 
-	if test -z "$oldrev"
+	if test -z "$1"
 	then
 		die "You must provide a revision to map"
+	fi
+
+	oldrev=$(git rev-parse --revs-only "$1") || exit $?
+	newrev=
+
+	if test -n "$2"
+	then
+		newrev=$(git rev-parse --revs-only "$2") || exit $?
 	fi
 
 	cache_setup || exit $?
 	cache_set "$oldrev" "$newrev"
 
 	say "Mapped $oldrev => $newrev"
+}
+
+# Usage: cmd_ignore REV...
+cmd_ignore () {
+	revs=$(git rev-parse $default --revs-only "$@") || exit $?
+	ensure_single_rev $revs
+
+	say "Ignoring $revs"
+
+	cache_setup || exit $?
+
+	git rev-list $revs |
+	while read rev
+	do
+		cache_set "$rev" ""
+	done
+
+	echo "$revs" >>"$cachedir/processed"
+}
+
+# Usage: cmd_use REV...
+cmd_use () {
+	revs=$(git rev-parse $default --revs-only "$@") || exit $?
+	ensure_single_rev $revs
+
+	say "Using existing subtree $revs"
+
+	cache_setup || exit $?
+
+	git rev-list $revs |
+	while read rev
+	do
+		cache_set "$rev" "$rev"
+	done
+
+	echo "$revs" >>"$cachedir/processed"
 }
 
 # Usage: cmd_split [REV]
@@ -949,7 +1005,7 @@ cmd_split () {
 		done || exit $?
 	fi
 
-	unrevs="$(find_existing_splits "$dir" "$rev")" || exit $?
+	unrevs="$(find_existing_splits "$dir" "$rev") $(exclude_processed_refs)" || exit $?
 
 	mainline="$(find_mainline_ref "$dir" "$revs")"
 	if test -n "$mainline"
