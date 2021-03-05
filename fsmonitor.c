6230ff1b6ac1c08625b0c62c482674302adfadfc
Johannes Schindelin <johannes.schindelin@gmx.de>
2021-03-05 19:41:19 +0100

    fixup! 64a32a6df6b0da05bc528ec70037c5f0af36066d

--- fsmonitor.c
+++ fsmonitor.c
@@ -149,16 +149,17 @@ void write_fsmonitor_extension(struct strbuf *sb, struct index_state *istate)
 /*
  * Call the query-fsmonitor hook passing the last update token of the saved results.
  */
-static int query_fsmonitor(int version, struct repository *r,
-			   const char *last_update, struct strbuf *query_result)
+static int query_fsmonitor(int version, struct index_state *istate, struct strbuf *query_result)
 {
+	struct repository *r = istate->repo ? istate->repo : the_repository;
+	const char *last_update = istate->fsmonitor_last_update;
 	struct child_process cp = CHILD_PROCESS_INIT;
 	int result;
 
-	if (!r)
-		r = the_repository;
+	if (!core_fsmonitor)
+		return -1;
 
-	if (r && r->settings.use_internal_fsmonitor > 0) {
+	if (r->settings.use_internal_fsmonitor > 0) {
 #ifdef HAVE_FSMONITOR_DAEMON_BACKEND
 		return fsmonitor_ipc__send_query(last_update, query_result);
 #else
@@ -168,9 +169,6 @@ static int query_fsmonitor(int version, struct repository *r,
 		return 0;
 #endif
 	}
-
-	if (!core_fsmonitor)
-		return -1;
 
 	strvec_push(&cp.args, core_fsmonitor);
 	strvec_pushf(&cp.args, "%d", version);
@@ -279,8 +277,7 @@ void refresh_fsmonitor(struct index_state *istate)
 	if (istate->fsmonitor_last_update) {
 		if (hook_version == -1 || hook_version == HOOK_INTERFACE_VERSION2) {
 			query_success = !query_fsmonitor(HOOK_INTERFACE_VERSION2,
-				istate->repo, istate->fsmonitor_last_update,
-				&query_result);
+				istate, &query_result);
 
 			if (query_success) {
 				if (hook_version < 0)
@@ -310,8 +307,7 @@ void refresh_fsmonitor(struct index_state *istate)
 
 		if (hook_version == HOOK_INTERFACE_VERSION1) {
 			query_success = !query_fsmonitor(HOOK_INTERFACE_VERSION1,
-				istate->repo, istate->fsmonitor_last_update,
-				&query_result);
+				istate, &query_result);
 		}
 
 		trace_performance_since(last_update, "fsmonitor process '%s'", core_fsmonitor);
