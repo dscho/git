eebce1c22cc2e8bd3af8bb8ca0d3b868e0f35710
Johannes Schindelin <johannes.schindelin@gmx.de>
2022-08-07 22:53:35 +0200

    fixup! ci: run `contrib/subtree` tests in CI builds
    Let's include the log in the output if the `subtree` tests fail.
    
    The downside of doing it this way is that the `subtree` tests won't even
    run if anything in the regular tests goes wrong, but it's the best we
    can do for now.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- ci/run-test-slice.sh
+++ ci/run-test-slice.sh
@@ -7,10 +7,13 @@
 
 group "Run tests" make --quiet -C t T="$(cd t &&
 	./helper/test-tool path-utils slice-tests "$1" "$2" t[0-9]*.sh |
-	tr '\n' ' ')" ||
-handle_failed_tests
+	tr '\n' ' ')" &&
 
 # Run the git subtree tests only if main tests succeeded
-test 0 != "$1" || make -C contrib/subtree test
+if test 0 = "$1"
+then
+	make -C contrib/subtree test
+fi ||
+handle_failed_tests
 
 check_unignored_build_artifacts
